5806cff7e34055009901267a5c292eeb4c26360f
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-10-13 00:13:58 +0300

    done

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,83 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const { waitForDebugger } = require("inspector");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+    return contacts;
+  } catch (error) {
+    console.error("Eroare la cititrea fisierului", error.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const filteredContacts = contacts.filter(
+      (element) => element.id === contactId
+    );
+    console.table(filteredContacts);
+    return filteredContacts;
+  } catch (error) {
+    console.error("Eroare la cititrea fisierului", error.message);
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index !== -1) {
+      contacts.splice(index, 1);
+      await fs.writeFile(contactsPath, JSON.stringify(contacts));
+      console.log("Utilizatorul a fost sters");
+    } else {
+      console.log("Utilizatorul nu a fost gasit!");
+    }
+  } catch (error) {
+    console.error("Eroare la cititrea fisierului", error.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const newContact = {
+      id: new Date(),
+      name: body.name,
+      email: body.email,
+      phone: body.phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.log("Utilizatorul a fost adaugat");
+    return contacts;
+  } catch (error) {
+    console.error("Eroare la cititrea fisierului", error.message);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    contacts[index] = { id: contactId, name, email, phone };
+    console.table(contacts);
+    return contacts;
+  } catch (error) {
+    console.error("Eroare la cititrea fisierului", error.message);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +85,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,103 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json({
+      status: "succes",
+      code: 200,
+      message: "Contacts are displayed successfully",
+      data: { ...contacts },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Eroare la preluarea task-urilor",
+    });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    res.status(200).json({
+      status: "succes",
+      code: 200,
+      message: "Contact is displayed successfully",
+      data: { ...contact },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Eroare la preluarea task-urilor",
+    });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const contact = req.body;
+  try {
+    const data = await addContact(contact);
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      message: "Contact added successfully",
+      data: { ...data },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Eroare la preluarea task-urilor",
+    });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    await removeContact(contactId);
+    res.status(204).json();
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Eroare la stergerea task-ului",
+    });
+  }
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const body = req.body;
+  try {
+    const data = await updateContact(contactId, body);
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      message: "Contact edited successfully",
+      data: { ...data },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Eroare la editarea task-ului",
+    });
+  }
+});
+
+module.exports = router;
